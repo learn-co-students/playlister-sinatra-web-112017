3700eee63e7b63675194689665637588d38a2b4b
ddbrennan <ddbren@gmail.com>
2017-12-14 17:04:55 -0500

    more stuff

---
+++ app/models/concerns/slugifiable.rb
@@ -0,0 +1,18 @@
+module Slugifiable
+
+  module InstanceMethods
+
+    def slug
+      self.name.downcase.gsub(' ','-')
+    end
+  end
+
+  module ClassMethods
+
+    def find_by_slug(slug)
+      name = slug.split("-").map(&:capitalize).join(" ")
+      self.find_by(name: name)
+    end
+  end
+
+end
